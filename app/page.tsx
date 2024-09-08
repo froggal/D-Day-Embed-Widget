dd2ea43d5ebaf5d8d77ef8ffac1a32062743da37
froggal <keyfrog@outlook.kr>
2024-09-08 18:41:46 +0900

    setup fonts and apply Pretendard Series fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+    const dDay = 0
+    const setYear = 0
+    const setMonth = 0
+    const setDay = 0
+    const eventName = 0
   return (
-    <>
-    </>
+    <div className={styles.container}>
+        <h1>D-{dDay}</h1>
+        <h2>{setYear}/{setMonth}/{setDay}</h2>
+        <h2>{eventName}</h2>
+    </div>
   );
 }
